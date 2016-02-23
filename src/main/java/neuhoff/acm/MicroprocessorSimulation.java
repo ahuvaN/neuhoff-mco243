7341d21e4874518d43c19bce55317478df8f0ce4
Ahuva <ahuvanewoff112@gmail.com>
2016-02-23 01:32:16 -0500

    updated processor

--- src/main/java/neuhoff/acm/MicroprocessorSimulation.java
+++ src/main/java/neuhoff/acm/MicroprocessorSimulation.java
@@ -12,46 +12,42 @@ public class MicroprocessorSimulation {
 	private int accA;
 	private int accB;
 
-	public MicroprocessorSimulation(String fileName, int lineNum) {
+	public MicroprocessorSimulation(String fileName) {
 		memory = new int[256];
 
 		try {
-			readInCode(fileName, lineNum);
+			readInCode(fileName);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void readInCode(String file, int lineNum) throws FileNotFoundException {
+	private void readInCode(String file) throws FileNotFoundException {
 		Scanner in = new Scanner(new File(file));
-		
-		String val = null;
-		do{
-			val = in.next();
-			lineNum--;
-		}while(lineNum > 0);
-		
-		String[] temp = val.split("");
-		for (int i = 0; i < 256; i++) {
-			memory[i] = convertHexToDec(temp[i]);
-		}
-		in.close();
-		translateCode();
-	}
 
-	
+		String val = null;
+		do {
+			val = in.next();
+			String[] temp = val.split("");
+			for (int i = 0; i < 256; i++) {
+				memory[i] = convertHexToDec(temp[i]);
+			}
+			in.close();
+			translateCode();
+			System.out.println();
+		} while (memory[0] != 8);
+		//System.exit(0);
+	}
 
 	private void translateCode() {
 		for (int i = 0; i < 256; i++) {
 			int val = memory[i];
 			switch (val) {
 			case 0:
-				accA = memory[memory[i + 1] * 16 + memory[i + 2]];
-				i += 2;
+				accA = memory[memory[++i] * 16 + memory[++i]];
 				break;
 			case 1:
-				memory[memory[i + 1] * 16 + memory[i + 2]] = accA;
-				i += 2;
+				memory[memory[++i] * 16 + memory[++i]] = accA;
 				break;
 			case 2:
 				int temp = accA;
@@ -77,21 +73,18 @@ public class MicroprocessorSimulation {
 				break;
 			case 6:
 				if (accA == 0) {
-					i = memory[memory[i + 1] * 16 + memory[i + 2]];
-					if (i != 0){
-						i--;
-					}
+					i = memory[i + 1] * 16 + memory[i + 2];
+					i--;
+				} else {
+					i += 2;
 				}
 				break;
 			case 7:
-				i = memory[memory[i + 1] * 16 + memory[i + 2]];
-				if (i != 0){
-					i--;
-				}
+				i = memory[i + 1] * 16 + memory[i + 2];
+				i--;
 				break;
 			case 8:
 				getOutput();
-				System.exit(0);
 			}
 		}
 	}
@@ -110,29 +103,41 @@ public class MicroprocessorSimulation {
 			return Integer.parseInt(val);
 		} else {
 			switch (val) {
-			case "A": return 10;
-			case "B": return 11;
-			case "C": return 12;
-			case "D": return 13;
-			case "E": return 14;
-			case "F": return 15;
+			case "A":
+				return 10;
+			case "B":
+				return 11;
+			case "C":
+				return 12;
+			case "D":
+				return 13;
+			case "E":
+				return 14;
+			case "F":
+				return 15;
 			}
 		}
 		return -1;
 	}
-	
+
 	private String convertDecToHex(int val) {
 
 		if (val >= 0 && val < 10) {
 			return String.valueOf(val);
 		} else {
 			switch (val) {
-			case 10: return "A";
-			case 11: return "B";
-			case 12: return "C";
-			case 13: return "D";
-			case 14: return "E";
-			case 15: return "F";
+			case 10:
+				return "A";
+			case 11:
+				return "B";
+			case 12:
+				return "C";
+			case 13:
+				return "D";
+			case 14:
+				return "E";
+			case 15:
+				return "F";
 			}
 		}
 		return null;
@@ -140,7 +145,6 @@ public class MicroprocessorSimulation {
 
 	public static void main(String[] args) {
 		String inputFile = "mach.in.txt";
-		int lineNum = 3;
-		MicroprocessorSimulation ms = new MicroprocessorSimulation(inputFile, lineNum);
+		MicroprocessorSimulation ms = new MicroprocessorSimulation(inputFile);
 	}
 }
